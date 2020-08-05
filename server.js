6c1bd145584a3b05f5c46c8a4c555e0ba7c70b3f
mlr425 <mattrod425@gmail.com>
2020-08-05 11:42:17 -0700

    init author routes

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
+//mongodb server pw user,ttq40Qq6OKIxOg5r
 if(process.env.NODE_ENV !== 'production'){
-    require('dontenv').config()
+    require('dotenv').config()
 }
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine','ejs')
 app.set('views',__dirname + '/views') //all of the different views for our server, will go here
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
-})
+// mongoose.connect(process.env.DATABASE_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+
+mongoose
+    .connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
 
 const db = mongoose.connection
 
@@ -27,6 +38,7 @@ db.once('error',() => console.log('connected to db'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
 
